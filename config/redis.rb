c8b5f8d105b47df0fa1c1fd663c520ceea4241b4
atul singh <atul.singh@shephertz.co.in>
2014-04-08 18:49:22 +0530

    Update redis.rb

--- config/redis.rb
+++ config/redis.rb
@@ -1,3 +1,3 @@
 # Point to your Redis server
-Redis.current = Redis.new(:host => '192.168.2.53', :port => 3694952840, :password => 'a73dv2zty6ve0k6zfoccekda82brgdt6a0bknjdmalqkyh2y3m0yo8jcu8gu9r10')
+Redis.current = Redis.new(:host => '192.168.2.53', :port => 52840, :password => 'a0bknjdmalqkyh2y3m0yo8jcu8gu9r10')
 #Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
